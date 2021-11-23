167e4198fc3b86ea90061230424090a876f5babb
Andrea <andrelomb2002@gmail.com>
2021-11-23 11:20:30 +0100

    nebriii

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from pyrogram.handlers import MessageHandler
 
 def dump(client, message):
     if message.text is not None:
-        print(message.text[message.text.find("kucoin.com/")+10:])
+        if "kucoin.com" in message.text:
+            print(message.text[message.text.find("kucoin.com/")+11:])
 
-app = Client("WeArePoor")
+app = Client("WeArePoor",14161945,"f0e1e792f0bc2ab1de43e39226eddf8d")
 
 app.add_handler(MessageHandler(dump))
 
